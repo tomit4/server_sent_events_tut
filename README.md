ef6c67060df3126b9da4c0556ce85f883159cde9
tomit4 <mosssap@gmail.com>
2023-02-05 23:13:45 -0800

    :tada: Initial commit!

---
+++ README.md
@@ -0,0 +1,4 @@
+### Server Sent Events Crash Course by Hussein Nasser
+
+A quick covering of the basics of server side events from Hussein Nasser's
+[Youtube Video on the subject](https://www.youtube.com/watch?v=4HlNv1qpZFY).
---
+++ index.js
@@ -0,0 +1,37 @@
+// Following a tutorial by youtuber Hussein Nasser to understand server side
+// events: https://www.youtube.com/watch?v=4HlNv1qpZFY
+
+const app = require("express")()
+const cors = require('cors')
+
+app.use(cors({
+    origin: true,
+    credentials: true
+}))
+
+app.get("/", (req, res) => {
+  res.send("hello!")
+})
+
+app.get("/stream", (req, res) => {
+    res.setHeader("Content-Type", "text/event-stream")
+    send(res)
+    // res.write("data: " + "hello!\n\n")
+})
+
+let i = 0
+function send(res) {
+    res.write("data: " + `hello${i++}!\n\n`)
+    setTimeout(() => send(res), 1000)
+}
+
+app.listen(8080)
+console.log("listening on 8080")
+
+// Navigate to http://localhost:8080 and enter into browser console:
+// let sse = new EventSource("http://localhost:8080/stream")
+// sse.onmessage = console.log
+
+// NOTE: If you kill the connection, the console will continue to print out the
+// res.write() above while it waits for the server to reconnect.
+// It is "as if" it is stateless.
