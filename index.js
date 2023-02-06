96708153e16ff8896be2cbf7802f6a7cfc2b2210
tomit4 <mosssap@gmail.com>
2023-02-05 23:29:33 -0800

    :memo: Made further notes on sse

--- index.js
+++ index.js
@@ -29,9 +29,17 @@ app.listen(8080)
 console.log("listening on 8080")
 
 // Navigate to http://localhost:8080 and enter into browser console:
+
 // let sse = new EventSource("http://localhost:8080/stream")
 // sse.onmessage = console.log
+
+// If you wish to close the connection, simply call:
+// sse.close()
 
 // NOTE: If you kill the connection, the console will continue to print out the
 // res.write() above while it waits for the server to reconnect.
 // It is "as if" it is stateless.
+
+// FURTHER READING:
+// https://developer.mozilla.org/en-US/docs/Web/API/EventSource
+// https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events
